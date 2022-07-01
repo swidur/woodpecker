e3edd7b5582eb03215e7877690e56fe3e35a78da
świdur <dawid@swidurski.pl>
2022-07-01 17:52:45 +0200

    Minifying and compression

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var favicon = require('serve-favicon')
 var path = require('path')
 var {query} = require('./db/index.js')
 var cors = require('cors')
+var compression = require('compression')
+var minify = require('express-minify');
 
 logger.setLevel('debug')
 
@@ -12,15 +14,18 @@ const app =express()
 app.use(cors())
 mountRoutes(app)
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use("/css", express.static(__dirname + '/node_modules/chessground/assets'));
-app.use("/js", express.static(__dirname + '/node_modules/chessground'));
-app.use("/js", express.static(__dirname + '/node_modules/chess.js'));
+app.use(compression())
+app.use(minify())
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico', { maxAge: 31557600 })))
+app.use("/css", express.static(__dirname + '/node_modules/chessground/assets', { maxAge: 31557600 }));
+app.use("/js", express.static(__dirname + '/node_modules/chessground', { maxAge: 31557600 }));
+app.use("/js", express.static(__dirname + '/node_modules/chess.js', { maxAge: 31557600 }));
 app.use("/js", express.static(__dirname + '/client/js'));
-app.use("/sounds", express.static(__dirname + '/client/sounds'));
-app.use("/js", express.static(__dirname + '/node_modules/sound-play/build'));
-app.use("/js", express.static(__dirname + '/node_modules/sound-play/src'));
-app.use("/js", express.static(__dirname + '/node_modules/axios/build'));
+app.use("/sounds", express.static(__dirname + '/client/sounds', { maxAge: 31557600 }));
+app.use("/js", express.static(__dirname + '/node_modules/sound-play/build', { maxAge: 31557600 }));
+app.use("/js", express.static(__dirname + '/node_modules/sound-play/src', { maxAge: 31557600 }));
+app.use("/js", express.static(__dirname + '/node_modules/axios/build', { maxAge: 31557600 }));
 var timerID = setInterval(function() {
   query('REFRESH MATERIALIZED VIEW concurrently hundredrandomids')
 }, 60 * 1000); 
